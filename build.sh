9319961bc3552655bfdd2f8168029dcadce5eeb2
Cupmouse <cupnmouse@gmail.com>
2019-06-01 11:07:09 +0900

    Created script for build and run Added nekonium logo image

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Please update apps/explorer/config/dev.secret.exs
+# And apps/block_scout_web/config/dev.secret.exs
+# Before running this script!
+# See https://forum.poa.network/t/manual-blockscout-deployment/2458
+
+# Parameter
+# $1 = database username
+# $2 = user password
+# $3 = database host (ex. localhost)
+# $4 = database name
+
+echo "Removing front end built files..."
+remove -r apps/block_scout_web/priv/static
+
+# Compile
+echo "Compiling..."
+mix do deps.get, local.rebar --force, deps.compile, compile
+
+# Database drop
+echo "Dropping databases..."
+mix do ecto.drop, ecto.create, ecto.migrate
+
+# Database create
+echo "Creating database..."
+mix do ecto.create, ecto.migrate
+
+# Installing and compiling web backend dependency
+echo "Installing nodejs dependency for backend and frontend..."
+cd apps/block_scout_web/assets; npm install && node_modules/webpack/bin/webpack.js --mode production; cd -
+cd apps/explorer && npm install; cd -
+
+SECRET_KEY=$(mix phx.gen.secret)
+
+echo "*** THIS MIGHT BE IMPORTANT!!! STORED phx.gen.secret=>secret.key"
+echo $SECRET_KEY > secret.key
+
+export DATABASE_URL="postgresql://$1:$2@$3/$4"
+
+MIX_ENV=prod mix release --env=prod
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Parameter
+# $1 = database username
+# $2 = user password
+# $3 = database host (ex. localhost)
+# $4 = database name
+
+export NETWORK="Nekonium"
+export SUBNETWORK="Mainnet"
+export NETWORK_ICON="_network_icon.html"
+export LOGO="/images/nekonium.svg"
+export ETHEREUM_JSONRPC_VARIANT="parity"
+export ETHEREUM_JSONRPC_HTTP_URL="localhost:8293"
+export ETHEREUM_JSONRPC_TRACE_URL="localhost:8293"
+export ETHEREUM_JSONRPC_WS_URL="ws://localhost:8294"
+export NETWORK_PATH=""
+export SECRET_KEY_BASE=$(cat secret.key)
+export CHECK_ORIGIN="false"
+export PORT=8080
+export COIN="NUKO"
+export DATABASE_URL="postgresql://$1:$2@$3/$4"
+export POOL_SIZE=20
+export ECTO_USE_SSL="true"
+export HEART_COMMAND="./run.sh"
+export FIRST_BLOCK=0
+
+MIX_ENV=prod mix phx.server
